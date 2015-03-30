51fba4bce092f3d578c5aa4c1aedebd3fde23962
hiroqn <hiroqn1008@gmail.com>
2015-03-30 15:22:58 +0900

    いったんPush

--- js/main.js
+++ js/main.js
@@ -3,55 +3,66 @@
  */
 var app = angular.module('fxosBgm', ['ngMaterial']);
 
-
-var testJSON = [{
-    album: "水星",
-    artist: "tofubeats",
-    artworkUrl: "http://is1.mzstatic.com/image/pf/us/r30/Music/v4/38/63/01/38630117-85a0-5307-d679-1d1d75539502/suiseijacket_mas.400x400-75.jpg",
-    mp3Url: null,
-    title: "水星(Original mix) [feat. オノマトペ大臣]",
-    trackUrl: "https://itunes.apple.com/jp/album/shui-xing-original-mix-feat./id535718064?i=535718218&uo=4"
-    },
-    {
-        album: "Don't Stop The Music",
-        artist: "tofubeats",
-        artworkUrl: "http://is1.mzstatic.com/image/pf/us/r30/Music4/v4/3d/fe/43/3dfe434e-9a79-a85e-723d-1b7a45f7c19d/825646365197.400x400-75.jpg",
-        mp3Url: null,
-        title: "Don't Stop The Music feat.森高千里",
-        trackUrl: "https://itunes.apple.com/jp/album/dont-stop-music-feat.-sen/id724083591?i=724083632&uo=4"
-    },
-    {
-        album: "lost decade (ボーナストラック バージョン)",
-        artist: "tofubeats",
-        artworkUrl: "http://is5.mzstatic.com/image/pf/us/r30/Music/v4/70/91/cf/7091cf2c-f0bb-e5fe-7727-6f5d95399628/tofu_lostdecade.400x400-75.jpg",
-        mp3Url: null,
-        title: "No.1 feat.G.RINA",
-        trackUrl: "https://itunes.apple.com/jp/album/no.1-feat.g.rina/id623204999?i=623205057&uo=4"
-    }];
-
-app.controller('MainCtrl', function ($scope, $element) {
+app.controller('MainCtrl', ['$scope', '$element', function ($scope, $element) {
     $scope.ctrlInfo = {
         level: 50,
-        isPlay: false
+        isPlay: false,
+        word: ''
     };
-    var context = new AudioContext();
+
+    //var context = new AudioContext();
     var audio = new Audio();
+    audio.volume = 0.5;
     audio.addEventListener('loadstart', function () {
-        var source = context.createMediaElementSource(audio)
-        source.connect(context.destination);
-        source.noteOn(0);
+        audio.play();
+        $scope.ctrlInfo.isPlay = true;
+    });
+    audio.addEventListener('ended', function () {
+        $scope.onPlayNext();
 
-        //var analyser = context.createAnalyser();
-        //audio.play()
-    })
+        api.convertRequest()
+            .then(function() {
+                // コンバート後
+            });
+    });
 //// url = URL.createObjectURL(file_or_blob)
-    audio.src = 'http://www.hmix.net/music/n/n46.mp3';
-    $scope.music = testJSON[Math.floor(testJSON.length * Math.random())];
+    $scope.music = {
+        album: "水星",
+            artist: "tofubeats",
+            artworkUrl: "http://is1.mzstatic.com/image/pf/us/r30/Music/v4/38/63/01/38630117-85a0-5307-d679-1d1d75539502/suiseijacket_mas.400x400-75.jpg",
+            mp3Url: null,
+            title: "水星(Original mix) [feat. オノマトペ大臣]",
+            trackUrl: "https://itunes.apple.com/jp/album/shui-xing-original-mix-feat./id535718064?i=535718218&uo=4"
+    };
 
+    $scope.$watch('ctrlInfo.level', function (newValue, oldValue) {
+        audio.volume = newValue / 100;
+    })
     $scope.onPlayOrPause = function () {
-
+        $scope.ctrlInfo.isPlay? audio.pause() : audio.play();
+        $scope.ctrlInfo.isPlay =!$scope.ctrlInfo.isPlay;
     };
     $scope.onPlayNext = function () {
+        count++;
+        $scope.music = api.playlist[count];
+        audio.src = api.playlist[count].mp3Url;
+    };
+    var count = 0;
 
-    }
-});
+    var api = new API();
+    $scope.onSearch = function () {
+        api.initWithTerm($scope.ctrlInfo.word)
+            .then(function(){
+                count = 0;
+                $scope.music = api.playlist[count];
+                audio.src = api.playlist[count].mp3Url;
+                //audio.play();
+                // api.playlistに5曲mp3が入っている状態
+            });
+        // 次の曲をコンバート
+        //api.convertRequest()
+        //    .then(function() {
+        //        // コンバート後
+        //    });
+    };
+}]);
